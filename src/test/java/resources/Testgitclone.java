2e6e679cbddc3506fb7d00e82777b840e3bb3a98
AviParate <avinash.parate.eth@gmail.com>
2026-03-08 14:31:06 +0530

    This is commit for CloneGITRestAssuredAPIFramework

---
+++ src/test/java/resources/Testgitclone.java
@@ -0,0 +1,17 @@
+package resources;
+
+public class Testgitclone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("poarjira");
+System.out.println("poarjira1");
+System.out.println("poarjira2");
+
+
+System.out.println("deletetwitter");
+System.out.println("deletetwitter2");
+System.out.println("deletetwitter3");
+	}
+
+}
