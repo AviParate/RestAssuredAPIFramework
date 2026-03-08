61931c38b75612921a67bb73d6a8932407ab4b52
AviParate <avinash.parate.eth@gmail.com>
2026-03-08 15:47:27 +0530

    m2

--- src/test/java/resources/Testgitclone.java
+++ src/test/java/resources/Testgitclone.java
@@ -8,12 +8,17 @@ System.out.println("poarjira");
 System.out.println("poarjira1");
 System.out.println("poarjira2");
 System.out.println("postjira3");
+System.out.println("postjira4");
+System.out.println("postjira5");
+System.out.println("postjira6");
 
 
 System.out.println("deletetwitter");
 System.out.println("deletetwitter2");
 System.out.println("deletetwitter3");
 System.out.println("deletetwitter4");
+System.out.println("deletetwitter5");
+System.out.println("deletetwitter6");
 	}
 
 }
